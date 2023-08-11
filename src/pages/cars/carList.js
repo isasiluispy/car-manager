5e13ef3bc5afece08de7990e71511380cff88f4b
Luis Isasi <isasiluispy@gmail.com>
2023-08-11 02:15:36 -0400

    cleaning code to improve readability and modularity

--- src/pages/cars/carList.js
+++ src/pages/cars/carList.js
@@ -7,20 +7,35 @@ const setCurrency = (value) => {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
 };
 
+const EditableCell = ({ value, index, keyName, type = "text", options = [], onChange }) => {
+  if (type === "select") {
+    return (
+      <Form.Select value={value} onChange={e => onChange(index, keyName, e.target.value)}>
+        {options.map((option, idx) => (
+          <option key={idx} value={option.value}>{option.label}</option>
+        ))}
+      </Form.Select>
+    );
+  }
+
+  return (
+    <Form.Control 
+      type={type} 
+      value={value} 
+      onChange={e => onChange(index, keyName, type === "number" ? Number(e.target.value) : e.target.value)} 
+    />
+  );
+}
+
 const CarList = () => {
   const [cars, setCars] = useState([]);
   const [isEditable, setIsEditable] = useState(false);
   const [copiedCars, setCopiedCars] = useState([]);
 
-  const getTotal = (car) => {
-    return car.productionCost + car.transportationCosts;
-  };
+  const getTotal = (car) => car.productionCost + car.transportationCosts;
 
   const handleClickOnUpdate = () => {
-    console.log('handleClickOnUpdate');
     setIsEditable(true);
-
-    // create a copy of cars array, the copy should be a whole different array
     const copiedCars = JSON.parse(JSON.stringify(cars));
     setCopiedCars(copiedCars);
   };
@@ -34,17 +49,13 @@ const CarList = () => {
   const handleSaveCopiedCars = () => {
     setIsEditable(false);
     setCars(copiedCars);
-
-    // Save cars to localStorage
     localStorage.setItem('cars', JSON.stringify(copiedCars));
   }
 
   useEffect(() => {
-    // Get cars from localStorage
     const storedCars = localStorage.getItem('cars');
-
-    setCars(JSON.parse(storedCars));  // Parse the string to convert back to array/object
-  }, []);  // The empty dependency array ensures this useEffect runs only once.
+    setCars(JSON.parse(storedCars) || []);  
+  }, []);
 
   return (
     <div>
@@ -57,14 +68,14 @@ const CarList = () => {
 
       <div className="mt-4">
         <div className="d-flex justify-content-between">
-          {isEditable ? 
-            (<>
+          {isEditable ? (
+            <>
               <Button className="px-4" variant="primary" onClick={() => setIsEditable(false)}>Cancel</Button>
-              <Button className="px-4" variant="primary" onClick={() => handleSaveCopiedCars()}>Save</Button>
-            </>) : 
-            (<Button className="px-4" variant="primary" onClick={() => handleClickOnUpdate()}>Update</Button>)
-          }
-          
+              <Button className="px-4" variant="primary" onClick={handleSaveCopiedCars}>Save</Button>
+            </>
+          ) : (
+            <Button className="px-4" variant="primary" onClick={handleClickOnUpdate}>Update</Button>
+          )}
         </div>
 
         <Table className="mt-4" striped bordered hover>
@@ -82,26 +93,12 @@ const CarList = () => {
           <tbody>
             {cars.map((car, index) => (
               <tr key={index}>
-                <td>{ isEditable ? <Form.Control type="text" value={copiedCars[index]['model']} onChange={e => handleInputChange(index, 'model', e.target.value)} /> : car.model }</td>
-                <td>{ isEditable ? 
-                  <Form.Select value={copiedCars[index]['brand']} onChange={e => handleInputChange(index, 'brand', e.target.value)}>
-                    {brands.map((brand, index) => (
-                      <option key={index} value={brand.value}>{brand.label}</option>
-                    ))}
-                  </Form.Select> : 
-                  car.brand }
-                </td>
-                <td>{ isEditable ? 
-                  <Form.Select value={copiedCars[index]['mainColor']} onChange={e => handleInputChange(index, 'mainColor', e.target.value)}>
-                    {mainColors.map((color, index) => (
-                      <option key={index} value={color.value}>{color.label}</option>
-                    ))}
-                  </Form.Select> : 
-                  car.mainColor }
-                </td>
-                <td>{ isEditable ? <Form.Control type="number" value={copiedCars[index]['value']} onChange={e => handleInputChange(index, 'value', Number(e.target.value))} /> : setCurrency(car.value) }</td>
-                <td>{ isEditable ? <Form.Control type="number" value={copiedCars[index]['productionCost']} onChange={e => handleInputChange(index, 'productionCost', Number(e.target.value))} /> : setCurrency(car.productionCost) }</td>
-                <td>{ isEditable ? <Form.Control type="number" value={copiedCars[index]['transportationCosts']} onChange={e => handleInputChange(index, 'transportationCosts', Number(e.target.value))} /> : setCurrency(car.transportationCosts) }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['model']} index={index} keyName="model" onChange={handleInputChange} /> : car.model }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['brand']} index={index} keyName="brand" type="select" options={brands} onChange={handleInputChange} /> : car.brand }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['mainColor']} index={index} keyName="mainColor" type="select" options={mainColors} onChange={handleInputChange} /> : car.mainColor }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['value']} index={index} keyName="value" type="number" onChange={handleInputChange} /> : setCurrency(car.value) }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['productionCost']} index={index} keyName="productionCost" type="number" onChange={handleInputChange} /> : setCurrency(car.productionCost) }</td>
+                <td>{ isEditable ? <EditableCell value={copiedCars[index]['transportationCosts']} index={index} keyName="transportationCosts" type="number" onChange={handleInputChange} /> : setCurrency(car.transportationCosts) }</td>
                 <td>{setCurrency(getTotal(car))}</td>
               </tr>
             ))}
